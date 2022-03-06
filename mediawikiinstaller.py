1a688ac53ca05abfe7d5eab892e702f40ed48045
Tyler887 <tylermageeshields@gmail.com>
2022-03-06 17:54:02 +0000

    Reformatting code with Black
    Generated code can be found at https://black.vercel.app/?version=stable&state=_Td6WFoAAATm1rRGAgAhARYAAAB0L-Wj4CNJCdtdAD2IimZxl1N_Wg2TFPiQOCcnAPXrjR9JW746LU_oMQrO7XA_ZxquCjyMpJviOJNlskzt6a7itKzDmXLxti3Kb9G6YkytRcuoOCJiL0qMGTEBbuCJLmEg2sXQk74YhpWH87BG1VHH_PQ-KBoTrvTuqe9SNBHocQSpahAn2R9L3RJUXVHhDrxPJETUVOe1ZG5ZVmFTKs4nVbNzbk73NfyiU6AELvMf-nb9jaJAWXkt2ZLvuy5dakhr1i-pxt5SFM4hUMz5S9bjE_uL5mMLsrgsRV1vZfWVEa72Ep6IhGSZuy0pbUtA4STwbYUahp7wEdUxYNECMM12YL7Frr7ljLKDeYRTK60lZVjkTmEYIIQHq84JXdNLWZvfNnOHFQ9jTDJ6-0TzQuxtAwdHTY42ULOhJA1-xCoR8YWWOwhzVFDZiJtEUOcf1fTcT5x6osWGvRLiU-4zWGALoaCqfot55a5x13fdOtc7IKkB59aci6wJ9ZTkcoXEpfqmZrh-MvY_djEquvcA7Ne_mbqOh-2ltgHa1fkY_VV7uiBbvcjuxlbTcwSV3dMkD2D9mV8PFgfihSksqGvEnosIqm9mRav3abG6sORoE5R-u60T9k9pGzMK-yVhF851FliCMcxsd5Ra0k3vumKZXiQjXW9VZGHOy7uWpYUFwZy26eiD0zIBxx2bPAxncy1B5zyKGbzBDsQJ8ZjwMujwLgvkNeDZpL_V1JSHZ2j5u1dBDdJcEr6Xup8Z7jxA-Bb8imqmSzK-XAWgPwFwqFcGVgFXDmB7azPhbFfT0JdSfk0wwg-bQrGPVstMSPCLLtdUzbrslJbP-mY_tuVQaE3DUxYB05TETChrsqjOYsiUe_9awm6OnYDSvkWjCyur31HEQeWm_7BJtNCOo-UyfYt7uUNuI7I0tWLhtjPYfpR9YoWnVKvsoWiUmV8tRClOZ_sFhIxzEmKVI-4HYUcajBd1oeWRvdanICqT0h9PU3iRGwHsSSFVPgE9ksM1gRIeyzHfIPZISvP2ebGSf_yLN2USkl6L-FQXcFMzlFH4o7UM0mBOHv4xtqP9WE5CYuA5Us3KkF32bsEbvudY519A35ONCpimTgIHoxBpx4Fn0lZHxeb6uaf-9bIhVw5s3Ikkmqoa6_c_7oKuWXv0wuR2WUhwTxWEECEP9ps-w7Ex-C4OKMPAXaG-8tQxThAfI_VEU2_3q-oe_CoSAxuQaMWw6IyWcbqjlIhq2awK_-42ogEoPhmajInvxGs7NXtBMn8LiFc6oGvZDtO7C6PbaeUrTANmS4TsAbB2fexiVCnza2xDUz6atOroLswqyavkFTJQEJUJhgMMiQrv1FheAxu-BYxOjVN9nMCYen48rmeybo38KIr0RkHfHKkA_fXIgdJTSIL9nQBUBDpRzttIs0OTFA0xCqYu6YhGqKbRLGMriTnYSqQDUSANKkIbZhn6qH4T7UBWA7bfeFGQ502WD2bNs7JiWZdy9rmNna80kUqZniAcAEkjLl-e6CXAnm70VKBBqzZXxJ9TYu4PXkHonrfOEmE1u6jL0LQAxtZxn_OthysOHWw46Mb8L6Rf1WNgBAjWNdJj7f1wyUl5iR3cvjms3n7El_Kbf3N5YcBtTAqkpvDblG2paBkAhKhNZBirMf7VV3QqIa_INNU-h7zIGuyCZev0J8W9hGRvoPLbQ8-RyBOnvkkFDxdlXOrBhsWreuzoUUU_-QH30N80B0h_zAQjv_2lJjUeLL6BWiR3JLdXH6aXPdH-mS2UV_4QInXMc2PNHnxqRzvPeR1nMZ894-xrLQyKZzczn1ctMb7fk5ZA6Ovd4-LthaAEsp9PvD_LKYcpSiG8ywJfYNAo4PZC8dc3-lJwSkYP-Wnczv_70iDFR6idusCbTtX-UyYwVvGhTy8riQMt4DjwLQhDKqdindNoy0FdWT_qTWZ4p2xZh_JOpTZM9daIZOVp_PGzTJbSOyWS-EM6q_4Fn2QkpuXnxl__lQWGSU7c8s6-m37_9irE940L4s0vVBzxYuheZsLAe8TvNqcNXEHAAkOFr6OC4Df21jFxBLJPZ-Fk3NA_FPNGWQeapS9BBr265fDtaiSJ5BUh4sSjONPf3lcY-fjpDopQW6MmDsAGgmUMt8PrhBDeAegFqu8_ChoxabP0VpWf5Hf70szqIjuXxEwE_8SLN2u1AVKexLe2Mn5ysOesFhnVepwFsxm4DdQNJfpM2Z3aQiLEvBhQtgoM6byCmteqmUZ7Rf8BEXf3Rup4sQxk0ND5EUUSAaACw6goZiSPWLbq9OiVCNOm-H_uQyGUNQYOhbvLdbiYKPtFXqNG7q4F-Nqi1IjlRdBBW3PxTIh9uBES1YYsun_ibf9eI2XY5oh-6y83ewNkvDoNjpA3JCw2L_BXfsAus7LcqAPYWuzWaY2SWaUFavv_g4jmcgMG90Unecgk5G_ynogHgfR5cdXzXX6H-81U7XQM8iooetJh7iAXCaonvGlNAeRvWDx2zE7627B4WK8r56cYOi11FARBlqUbF1C6uvHbVbrEnb9TOsiQjvvsS-3KOdgzhit1PX0GP8jpOxZ6vNCJiYtEuS0hN7Dn5IeY0Q88mCB4kC1zok_X8Is7ihdQdr38DtUt1TXNOacDKwC1j8SNtCyyzzLg3-dFuhRIkyQxD6K-EJnJ8v8WzxLn1Id7Bb1pyqhAtbFjZu7zSYSZgQIGs_tM5kWQOuxvSynstumwUWB9Orc9NjFEABEXt0_AhUuQZ378DTMZJnNOhsO6QyXus90W3k_T8DABkBYaK3WjQZbR24pdXv0sFyJpOXouN21naq6a2K9-3sPOQRn_ThSdb1rzN0EMgAkuZ861KEc5ZvXKAg9boYtQzg2zYyYpk1lVlzGu7JXgyfgIflptEG_bd7VZGXXwMtcmwEDEZLXo0WyN5gZ9F_dYdSw-x-iprQq2d70H0gUqP5aDiU6-Wr0jrZpuPvj6k4blH8Zm1OdLvi2DTuJSC71nESAXktunuVpOyTbDnqZiEh7IYFY_4ZFu_2rWuRZNC6uxIoageQOXoOfVr0EqdBBDAOzSRZ7CaEYQnk8fXwEhQS85Rdn48Sbvxi9LnVFvIMif3Te-nr-frJ419oeRsrck2jXGFOavB6LZlydOmwYBLAdK43633A1jRDWnwyTl15xMT4GOSEQIWRDb9eX0j5XbQ9kQPNiGGrJpsv0LliE97KU7L6Bqm1bTe2YDhOyhu91WgN2kx98iYR8zxJ-fY1d30OvY_7iwpxhyql_sT3OOEjiw9z2BP7pgX99R9DV3vRqeJpj_rup7OFYVad16QW607U-YqEWfXJHaSf0LuxTPf4GiFTNrHel598FAZL0Is8v44Ve6tMY9Zhu-LiTlAAAAIlx2awVxvDkAAfcTykYAAD7vZYGxxGf7AgAAAAAEWVo=

--- mediawikiinstaller.py
+++ mediawikiinstaller.py
@@ -1,18 +1,18 @@
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-#          MᴇᴅɪᴀWɪᴋɪ Iɴsᴛᴀʟʟᴇʀ Pʀᴏɢʀᴀᴍ              #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Purpose: Helps you instantly and automatically    #
-#          install MediaWiki (because Wikimedia     #
-#          wouldn't).                               #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# License: GPL-3.0-or-later                         #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# 𝗪𝗔𝗥𝗡𝗜𝗡𝗚: 𝗜 𝗗𝗢 𝗡𝗢𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗔𝗡𝗬 𝗪𝗔𝗥𝗥𝗔𝗡𝗧𝗬 𝗙𝗢𝗥   #
-# 𝗧𝗛𝗜𝗦 𝗦𝗖𝗥𝗜𝗣𝗧. 𝗨𝗦𝗘 𝗜𝗧 𝗔𝗧 𝗬𝗢𝗨𝗥 𝗢𝗪𝗡 𝗥𝗜𝗦𝗞.            #
-# I always try to fix any bugs, but sometimes it is #
-# impossible. You should inspect the source code    #
-# yourself.                                         #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#          MᴇᴅɪᴀWɪᴋɪ Iɴsᴛᴀʟʟᴇʀ Pʀᴏɢʀᴀᴍ               #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Purpose: Helps you instantly and automatically     #
+#          install MediaWiki (because Wikimedia      #
+#          wouldn't).                                #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# License: GPL-3.0-or-later                          #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# 𝗪𝗔𝗥𝗡𝗜𝗡𝗚: 𝗜 𝗗𝗢 𝗡𝗢𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗔𝗡𝗬 𝗪𝗔𝗥𝗥𝗔𝗡𝗧𝗬 𝗙𝗢𝗥    #
+# 𝗧𝗛𝗜𝗦 𝗦𝗖𝗥𝗜𝗣𝗧. 𝗨𝗦𝗘 𝗜𝗧 𝗔𝗧 𝗬𝗢𝗨𝗥 𝗢𝗪𝗡 𝗥𝗜𝗦𝗞.             #
+# I always try to fix any bugs, but sometimes it is  #
+# impossible. You should inspect the source code     #
+# yourself.                                          #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 #####################################################
 ####################### INIT ########################
@@ -21,180 +21,207 @@ import os
 import sys
 import ctypes
 import time
+
 try:
-  from colorama import *
+    from colorama import *
 except ImportError:
-  os.system("python -m pip install colorama")
-  from colorama import *
+    os.system("python -m pip install colorama")
+    from colorama import *
 try:
-  import inquirer
+    import inquirer
 except:
-  os.system("python -m pip install inquirer")
-  import inquirer
+    os.system("python -m pip install inquirer")
+    import inquirer
 from pathlib import Path
+
 home = str(Path.home())
 backtoinst = "N"
-print(f"{Fore.GREEN}MediaWiki installer{Style.RESET_ALL} (unofficial) developer release")
-print(f"{Fore.RED}Disclaimer:{Style.RESET_ALL} Wikimedia does NOT own this installer. Plus,")
+print(
+    f"{Fore.GREEN}MediaWiki installer{Style.RESET_ALL} (unofficial) developer release"
+)
+print(
+    f"{Fore.RED}Disclaimer:{Style.RESET_ALL} Wikimedia does NOT own this installer. Plus,"
+)
 print("            MediaWiki is PHP, so it will be installed.")
 import platform
 import shutil
 
 try:
-  import requests
+    import requests
 except ImportError:
-  os.system("python -m pip install requests")
-  import requests
+    os.system("python -m pip install requests")
+    import requests
 sysver = platform.system() + " " + platform.release() + " v" + platform.version()
 print(f"Running on: {sysver}")
 if os.name == "nt":
+
     def is_admin():
         try:
             return ctypes.windll.shell32.IsUserAnAdmin()
         except:
             return False
+
+
 def minstall():
- if not Path(f'{home}/MediaWiki').is_dir():
-   os.makedirs(f"{home}/MediaWiki")
- else:
-   if backtoinst == "Y":
-     file_path = f"{home}/MediaWiki"
-     try:
-         if os.path.isfile(file_path) or os.path.islink(file_path):
-             os.unlink(file_path)
-         elif os.path.isdir(file_path):
-             shutil.rmtree(file_path)
-         if os.name == "nt":
-             os.system("scoop reset apache php/php7.4 sqlite")
-     except Exception as e:
-         print('Failed to delete %s. Reason: %s' % (file_path, e))
-         exit(1)
-   else:
-     print(f"{Fore.YELLOW}Warning:{Style.RESET_ALL} MediaWiki is already installed! Trying to install MediaWiki again after installing it may\n{Fore.RED}clear your wiki{Style.RESET_ALL}. Proceed with caution.")
-     questions = [
-        inquirer.Confirm("confirm", message="Are you sure you want to reinstall MediaWiki?", default=False),
-     ]
-     rianswer = inquirer.prompt(questions)
-     if str(rianswer) == str("{'confirm': True}"):
-       file_path = f"{home}/MediaWiki"
-       try:
-         if os.path.isfile(file_path) or os.path.islink(file_path):
-             os.unlink(file_path)
-         elif os.path.isdir(file_path):
-             shutil.rmtree(file_path)
-         if os.name == "nt":
-             os.system("scoop reset apache php/php7.4 sqlite")
-       except Exception as e:
-         print('Failed to delete %s. Reason: %s' % (file_path, e))
-         exit(1)
-     else:
-       exit()
- print("These will be installed:")
- print("    1: Database (SQLite)")
- print("    2: MediaWiki")
- print("    3: Server (Apache)")
- print("    4: PHP")
- os.system("powershell -c Set-ExecutionPolicy RemoteSigned -scope CurrentUser")
- os.system("powershell -c Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')")
- os.system("scoop bucket add php")
- if is_admin():
-      print("> scoop bucket add extras")
-      os.system("scoop bucket add extras")
-      print("> scoop install php/php7.4 sqlite apache extras/vcredist2019")
-      os.system("scoop install php/php7.4 sqlite apache extras/vcredist2019")
- else:
-      print("> scoop install php/php7.4 sqlite apache")
-      os.system("scoop install php/php7.4 sqlite apache")
- if os.name != "nt":
-     print("See the MediaWiki documentation for the MediaWiki installation requirements:\nhttps://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Installation_requirements")
- link = "https://releases.wikimedia.org/mediawiki/1.37/mediawiki-1.37.1.zip"
- file_name = f"{home}/mediawiki.zip"
- with open(file_name, "wb") as f:
-     print("Downloading MediaWiki...")
-     response = requests.get(link, stream=True)
-     total_length = response.headers.get('content-length')
-     if total_length is None: # no content length header
-        f.write(response.content)
-     else:
-        dl = 0
-        total_length = int(total_length)
-        for data in response.iter_content(chunk_size=4096):
-            dl += len(data)
-            f.write(data)
-            done = int(50 * dl / total_length)
-            sys.stdout.write("\r[%s%s]" % ('#' * done, ':' * (50-done)) )    
-            sys.stdout.flush()
- new = f"{home}/MediaWiki"
- print(" Done!\n")
- import zipfile
- with zipfile.ZipFile(file_name, 'r') as zip_ref:
-     zip_ref.extractall(new)
- os.unlink(file_name)
- if os.name == "nt":
-   httpdfile = f"{home}\httpd.conf"
-   print("Configuring Apache HTTP Server...")
-   with open(f"{home}\MediaWiki\httpd.conf", "w") as f:
-      f.write(f"Include {home}\MediaWiki\mediawiki-1.37.1\*.php")
-   print(f"Start the server by running: httpd -f {home}\MediaWiki\httpd.conf")
-   time.sleep(2)
+    if not Path(f"{home}/MediaWiki").is_dir():
+        os.makedirs(f"{home}/MediaWiki")
+    else:
+        if backtoinst == "Y":
+            file_path = f"{home}/MediaWiki"
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+                if os.name == "nt":
+                    os.system("scoop reset apache php/php7.4 sqlite")
+            except Exception as e:
+                print("Failed to delete %s. Reason: %s" % (file_path, e))
+                exit(1)
+        else:
+            print(
+                f"{Fore.YELLOW}Warning:{Style.RESET_ALL} MediaWiki is already installed! Trying to install MediaWiki again after installing it may\n{Fore.RED}clear your wiki{Style.RESET_ALL}. Proceed with caution."
+            )
+            questions = [
+                inquirer.Confirm(
+                    "confirm",
+                    message="Are you sure you want to reinstall MediaWiki?",
+                    default=False,
+                ),
+            ]
+            rianswer = inquirer.prompt(questions)
+            if str(rianswer) == str("{'confirm': True}"):
+                file_path = f"{home}/MediaWiki"
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                    if os.name == "nt":
+                        os.system("scoop reset apache php/php7.4 sqlite")
+                except Exception as e:
+                    print("Failed to delete %s. Reason: %s" % (file_path, e))
+                    exit(1)
+            else:
+                exit()
+    print("These will be installed:")
+    print("    1: Database (SQLite)")
+    print("    2: MediaWiki")
+    print("    3: Server (Apache)")
+    print("    4: PHP")
+    os.system("powershell -c Set-ExecutionPolicy RemoteSigned -scope CurrentUser")
+    os.system(
+        "powershell -c Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')"
+    )
+    os.system("scoop bucket add php")
+    if is_admin():
+        print("> scoop bucket add extras")
+        os.system("scoop bucket add extras")
+        print("> scoop install php/php7.4 sqlite apache extras/vcredist2019")
+        os.system("scoop install php/php7.4 sqlite apache extras/vcredist2019")
+    else:
+        print("> scoop install php/php7.4 sqlite apache")
+        os.system("scoop install php/php7.4 sqlite apache")
+    if os.name != "nt":
+        print(
+            "See the MediaWiki documentation for the MediaWiki installation requirements:\nhttps://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Installation_requirements"
+        )
+    link = "https://releases.wikimedia.org/mediawiki/1.37/mediawiki-1.37.1.zip"
+    file_name = f"{home}/mediawiki.zip"
+    with open(file_name, "wb") as f:
+        print("Downloading MediaWiki...")
+        response = requests.get(link, stream=True)
+        total_length = response.headers.get("content-length")
+        if total_length is None:  # no content length header
+            f.write(response.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                sys.stdout.write("\r[%s%s]" % ("#" * done, ":" * (50 - done)))
+                sys.stdout.flush()
+    new = f"{home}/MediaWiki"
+    print(" Done!\n")
+    import zipfile
+
+    with zipfile.ZipFile(file_name, "r") as zip_ref:
+        zip_ref.extractall(new)
+    os.unlink(file_name)
+    if os.name == "nt":
+        httpdfile = f"{home}\httpd.conf"
+        print("Configuring Apache HTTP Server...")
+        with open(f"{home}\MediaWiki\httpd.conf", "w") as f:
+            f.write(f"Include {home}\MediaWiki\mediawiki-1.37.1\*.php")
+        print(f"Start the server by running: httpd -f {home}\MediaWiki\httpd.conf")
+        time.sleep(2)
+
 
 def byemw():
-  if os.path.exists(f"{home}/MediaWiki"):
-      try:
-        file_path = f"{home}/MediaWiki"
-        if os.name == "nt":
-           print("Telling Apache to shutdown the wiki server...")
-           os.system("httpd -k shutdown")
-           print("Uninstalling software...")
-           if is_admin():
-             os.system("scoop uninstall php/php7.4 sqlite apache extras/vcredist2019")
-           else:
-             os.system("scoop uninstall php/php7.4 sqlite apache")
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
-      except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
+    if os.path.exists(f"{home}/MediaWiki"):
+        try:
+            file_path = f"{home}/MediaWiki"
+            if os.name == "nt":
+                print("Telling Apache to shutdown the wiki server...")
+                os.system("httpd -k shutdown")
+                print("Uninstalling software...")
+                if is_admin():
+                    os.system(
+                        "scoop uninstall php/php7.4 sqlite apache extras/vcredist2019"
+                    )
+                else:
+                    os.system("scoop uninstall php/php7.4 sqlite apache")
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
+            exit(1)
+    else:
+        print("MediaWiki is not installed.")
         exit(1)
-  else:
-     print("MediaWiki is not installed.")
-     exit(1)
 
 
 print("Choose a mode:")
-print(f"    1: {Fore.GREEN}Install{Style.RESET_ALL} MediaWiki 1.37 and the required software")
+print(
+    f"    1: {Fore.GREEN}Install{Style.RESET_ALL} MediaWiki 1.37 and the required software"
+)
 print(f"    2: {Fore.RED}Uninstall{Style.RESET_ALL} the current MediaWiki")
 print("Or, type 'q' to quit")
-       ### Choose Mode ## 
+### Choose Mode ##
 try:
-  while True:
-    mode = "0"
-    mode = input("\nChoose mode: ")
-    if mode == "1":
-      minstall()
-    elif mode == "2":
-      byemw()
-    elif mode == "q":
-      exit()
-    else:
-      print(f"Invalid input: {mode}. Please choose 1 or 2. Or, type 'q' to quit.")
+    while True:
+        mode = "0"
+        mode = input("\nChoose mode: ")
+        if mode == "1":
+            minstall()
+        elif mode == "2":
+            byemw()
+        elif mode == "q":
+            exit()
+        else:
+            print(f"Invalid input: {mode}. Please choose 1 or 2. Or, type 'q' to quit.")
 # Ctrl+C confirmation
 except KeyboardInterrupt:
-   questions = [
-      inquirer.Confirm("confirm", message="Are you sure you want to exit?", default=False),
-   ]
-   answer = inquirer.prompt(questions)
-   if str(answer) == str("{'confirm': True}"):
-      exit()
-   else:
-    if mode == "1":
-      backtoinst = "Y"
-      minstall()
-    elif mode == "2":
-      byemw()
+    questions = [
+        inquirer.Confirm(
+            "confirm", message="Are you sure you want to exit?", default=False
+        ),
+    ]
+    answer = inquirer.prompt(questions)
+    if str(answer) == str("{'confirm': True}"):
+        exit()
     else:
-      scriptfile = __file__
-      os.system(f"python3 {scriptfile}")
-      
-      exit()
+        if mode == "1":
+            backtoinst = "Y"
+            minstall()
+        elif mode == "2":
+            byemw()
+        else:
+            scriptfile = __file__
+            os.system(f"python3 {scriptfile}")
+
+            exit()
